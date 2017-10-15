7e49259c8e3c9206106c7d35a09e4fe0a9da4a74
Dorian Kcira <dkcira@gmail.com>
2017-10-14 19:02:30 -0700

    instance type

--- create.sh
+++ create.sh
@@ -2,5 +2,8 @@
 
 # create a list of instances
 
+#INSTANCE_TYPE=f1-micro
+INSTANCE_TYPE=n1-standard-4
+
 INSTANCE_LIST=$(echo fdt{1..40})
-echo gcloud compute instances create ${INSTANCE_LIST} --async --machine-type=f1-micro --image-family=centos-7 --image-project=centos-cloud --metadata-from-file startup-script=startup-script.sh
+echo gcloud compute instances create ${INSTANCE_LIST} --async --machine-type=${INSTANCE_TYPE} --image-family=centos-7 --image-project=centos-cloud --metadata-from-file startup-script=startup-script.sh
